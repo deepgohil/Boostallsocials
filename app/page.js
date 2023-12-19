3737772dda9f4c20c9e81ce3d9322772d8e83214
deepgohil <20601106monisha@viva-technology.org>
2023-12-19 13:57:49 +0000

    pages change done

--- app/page.js
+++ app/page.js
@@ -40,65 +40,7 @@ const Home = () => {
        BUY NOW
       </button>
 
-      {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center z-100">
-          <div className="absolute inset-0 " />
-          <div className="bg-white flex flex-col p-8 w-[600px] rounded-lg z-10">
-            <h2 className="text-3xl text-center font-bold">Enter Values</h2>
-            <br />
-            <p>Enter values to get AI Recommended crops for your farm</p>
-            <br />
-            <div className="flex flex-row justify-around items-center">
-              <input
-                className="border max-w-[200px] px-2 py-1 rounded-lg my-2"
-                type="text"
-                placeholder="Latitude"
-                value={latitude}
-                disabled
-              ></input>
-              <input
-                className="border max-w-[200px] px-2 py-1 rounded-lg my-2"
-                type="text"
-                placeholder="Longitude"
-                value={longitude}
-                disabled
-              ></input>
-              <button
-                className="bg-blue-500 w-fit h-fit px-3 rounded-lg py-1 text-white"
-                onClick={getLocation}
-              >
-                Get Location
-              </button>
-            </div>
-            <input
-              className="border px-2 py-1 rounded-lg my-2"
-              type="text"
-              placeholder="Nitrogen (N)"
-            ></input>
-            <input
-              className="border px-2 py-1 rounded-lg my-2"
-              type="text"
-              placeholder="Phosphorus (P)"
-            ></input>
-            <input
-              className="border px-2 py-1 rounded-lg my-2"
-              type="text"
-              placeholder="Potassium (K)"
-            ></input>
-            <input
-              className="border px-2 py-1 rounded-lg my-2"
-              type="text"
-              placeholder="Soil pH"
-            ></input>
-            <button
-              onClick={() => setIsModalOpen(false)}
-              className="bg-red-500 hover:bg-red-600 text-white font-semibold mt-4 py-2 px-6 rounded-full w-fit"
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      )}
+
     </section>
   );
 };
